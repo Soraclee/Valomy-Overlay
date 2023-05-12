14e89deb4718c7c0c1b518b2d1f1ae3586fb3e59
Soracle <soso17pro@gmail.com>
2023-05-12 10:21:35 +0200

    Premier commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+var indexRouter = require("./routes/index");
+
+var path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
+
+app.use("/", indexRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
